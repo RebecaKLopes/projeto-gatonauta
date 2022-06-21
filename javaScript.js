255c5a709f02170aba1f1fa1f09e2fcf2350bf57
Rebeca Kethelyn Lopes <rebecasilva1406@gmail.com>
2022-06-21 12:50:02 -0300

    atualização

--- javaScript.js
+++ javaScript.js
@@ -1,9 +1,10 @@
 function entrar(){
     var login = document.getElementById('email').value;
     var senha = document.getElementById('senha').value;
+    
 
     if(login == "admin" && senha == "123"){
-        //location.href = "estilo/home.html";
+        //location.href = "home.html";
         alert('Por enquanto é só, aguarde as novas atualizações.')
     } else{
         alert('Usuário ou senha incorretos')
